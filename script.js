af3748b862af99358a88c32268dc9962db20558c
vamsi <vamcyellapu@gmail.com>
2023-05-20 06:38:20 +0530

    redo

--- script.js
+++ script.js
@@ -3,35 +3,36 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+
 const createIcon = (classes) => {
   const icon = document.createElement("i");
   icon.className = classes;
   return icon;
 };
+const createButton = (classes) => {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+};
 const addItem = (e) => {
   e.preventDefault();
+  // Validate Input
   const newItem = itemInput.value;
-  if (newItem.value === "") {
+  if (newItem === "") {
     alert("Please add an item");
     return;
   }
 
+  // Create list item
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
-
-  const createButton = (classes) => {
-    const button = document.createElement("button");
-    const icon = createIcon("fa-solid fa-xmark");
-    button.appendChild(icon);
-    button.className = classes;
-
-    return button;
-  };
-
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   itemList.appendChild(li);
+  itemInput.value = "";
 };
+// Event Listeners
 
-// Event listeners
 itemForm.addEventListener("submit", addItem);
